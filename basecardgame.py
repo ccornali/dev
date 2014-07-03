90b73c97dd5f25e5815024eabe67e319f6ae5317
ccornali <charleyncc@ucla.edu>
2014-07-03 13:00:53 -0700

    program objects simplified and code adjusted

--- basecardgame.py
+++ basecardgame.py
@@ -1,42 +1,65 @@
 #basecardgame.py
 import random
+from enum import Enum
 
-def convert_player_input(p_input):
-    p_input.lower()
+class Suit(Enum):
+    """Enum class for card suit"""
 
-    if p_input == 'j' or p_input == 'jack'
-        p_input = 'J'
-    if p_input == 'q' or p_input == 'queen'
-        p_input = 'Q'
-    if p_input == 'k' or p_input == 'king'
-        p_input = 'K'
-    if p_input == 'a' or p_input == 'aces'
-        p_input = 'A'
+    SPADES = 1
+    CLUBS = 2
+    HEARTS = 3
+    DIAMONDS = 4
 
-    if p_input == 'c' or p_input == 'clubs'
-        p_input = 'CLUBS'
-    if p_input == 'd' or p_input == 'diamonds'
-        p_input = 'DIAMONDS'
-    if p_input == 'h' or p_input == 'hearts'
-        p_input = 'HEARTS' 
-    if p_input == 's' or p_input == 'spades'
-        p_input = 'SPADES'
-    return p_input
-
-rank_str = (None,'ACE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE','TEN','JACK','QUEEN','KING')
-
-suit_str = (None,'SPADES','CLUBS','HEARTS','DIAMONDS')
+    def __init__(self):
+        # creates suit of card
+        self._suit = suit
+        if not isinstance(self._suit, basestring):
+            raise TypeError
+            print "Invalid input!"
     
-Suit = Enum('Suit', 'SPADES CLUBS HEARTS DIAMONDS')
-Rank = Enum('Rank', 'ACE TWO THREE FOUR FIVE SIX SEVEN EIGHT NINE TEN JACK QUEEN KING')
+    def __str__(self):
+        return "%s" % self._suit
+
+class Rank(Enum):
+    """Enum class for card rank """
+
+    ACE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+
+    def __init__(self):
+        # creates rank of card
+        self._rank = rank
+        if not isinstance(self._rank, basestring):
+            raise TypeError
+            print "Invalid input!"
+
+    def __str__(self):
+        return "%s" % self._rank
+#rank_str = [None,'ACE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE','TEN','JACK','QUEEN','KING']
+
+#suit_str = [None,'SPADES','CLUBS','HEARTS','DIAMONDS']
+
+#Suit = Enum('Suit', 'UNKNOWN SPADES CLUBS HEARTS DIAMONDS')
+#Rank = Enum('Rank', 'ACE TWO THREE FOUR FIVE SIX SEVEN EIGHT NINE TEN JACK QUEEN KING')
 
 class Card(object):
     """ represents a standard plating card"""
 
-    def __init__(self, rank = 1, suit = 0):
+    def __init__(self, rank = 1, suit = 1):
         # creates card object
-        self._rank = rank
-        self._suit = suit
+        self._rank = Rank
+        self._suit = Suit
 
     def __repr__(self):
         """ Returns readable string representation"""
@@ -44,7 +67,7 @@ class Card(object):
 
     def __eq__(self, other):
         """Compares two cards for equality"""
-        if not isinstance(other, Card)
+        if not isinstance(other, Card):
             return False
         return self._rank == other.rank and self._suit == other.suit
 
@@ -63,10 +86,14 @@ class Deck(object):
     def make_basic_deck(num_decks = 1):
         deck = Deck()
         # creates basic a basic deck with certain number of decks
+        # [(s, r) for x in Suit for y in Rank]
+            # deck.insert_card(Card(Rank, Suit))
+        #for i in Suit:
+            #for v in Rank:
         for i in range(num_decks):
             for suit in range(4):
                 for rank in range(1, 14):
-                    deck.insert_card(Card(rank, suit))
+                    deck.insert_card(Card(Rank, Suit))
         return deck
 
     def shuffle(self):
@@ -75,36 +102,41 @@ class Deck(object):
 
     def draw(self, hands):
         # deals card from deck
-        if self.check_deck_size() == 0:
-            raise IndexError: print "No More Cards in Deck"
+        if self.check_deck_size():
+            raise IndexError
+            print "No More Cards in Deck"
         return self._deck.pop()
 
     def check_deck_size(self):
         # return length of deck or size of deck
         # if then statement to test of cards are left in deck
             if not self._deck:
-                deck_len = 0
+                return False
             else:
-                deck_len = len(self._deck)
-            return deck_len
+                return True
+
+    def gen_ran_cardrank(self):
+        # generates random card rank
+        ran_cardrank = random.choice(Rank)
+        return ran_cardrank
 
     def insert_card(self, card):
         self._deck.append(card)
 
 class Table(object):
     # Table keeps track of players, their moves, announces them, and....
-    def __init__(self, players):
+    def __init__(self, players, num_players = 2):
         self._players = players
-        for i in enumerate(self._players):
-            self.
+        for i in range(len(num_players)):
+            self._players.append()
+        all_players = list(enumerate(self._players))
+
+    def get_all_players(self):
+        return all_players
 
     def announce(self, message, player):
         # tells all players a message
-        
         pass
-
-    def play_again(self):
-        if 
 
 def play_go_fish(table):
     # creates Go Fish game and had all the logic
@@ -116,51 +148,45 @@ def play_go_fish(table):
             player.receive_card(deck.draw())
     
     # places players at table
-    for player in players:
-        player.sit_at_table(table)
-
-    # chooses random player to start 
-    players_order 
+    #for player in players:
+    #    player.sit_at_table(table)
   
     # start to play the game
-    
-    for play_order in range(len(players_order)):
-        i = 0
-        while True:
+    i = 0
+    while True:
         # if match found remove card from player[i] hand and give to player[j] hand
-            move = player[i].get_next_move(Card(rank,suit), player[j])
+        opponent_player = random.choice(Table.get_all_players())
+        while opponent_player == player[i]:
+            opponent_player = random.choice(Table.get_all_players())
+            
+        ran_cardrank = deck.gen_ran_cardrank()
+        cards = Card(ran_cardrank, 0)
 
-            if move == "hey %s, do you have any %s?", (player[j], card) :
-                cards = player[j].give_card_to_player(Card(rank, suit))
-                if cards is None:
-                    player[i].receive_card(deck.draw())
-                    print "Go Fish, %s take card from deck" % player[i]
-                else:
-                    player[i].recieve_card(cards)
-            i = (i + 1) % len(players)
+        opponent_player_hand = opponent_player.get_player_hand()
+        
+        for cards in opponent_player_hand[:]:
+            get_cards = opponent_player.remove_cards_from_hand(cards)
+            player[i].receive_card(get_cards)
+            if cards is None:
+                print "Go Fish, %s take card from deck" % player[i] 
+        i = (i + 1) % len(players)
 
         if deck.check_deck_size == 0:
             print "Game over!"
-            winners = 0
-            for p in players:
-                if player[p].count_books > player[p + 1].count_books:
-                    print "Player %s wins!" % player[i]
-                    break
-                else:
-                    player[p].count_books == player[p + 1].count_books:
-                    winners += 1
-                    if winners == 2:
-                        print "Tie! Players %s and %s wins!" % (player[p], player[p + 1])
-                    if winners > 2:
-                        print "More than two players tied, cannot determine winner"
-                play_again = new_input("Do you wish to play again?(y or n) ")
-                count = 0
-                if player[p].response(play_again) == 'y':
-                        count += 1
+            # winner is the player with the most books
 
+            best_player = None
+            best_book_count = 0
+            for player in players:
+                if player.count_books() > best_book_count:
+                    best_player = player
+                    best_book_count = player.count_books()
+
+            sorted_players = sorted(players, cmp=lambda x, y: x.count_books() > y.count_books())
+            print "List of Finishing Places (Win to Lose): %s" % sorted_players
 
 class Player(Deck):
-""" class of players that play Go Fish"""
+    """ class of players that play Go Fish"""
 
     def __init__(self, name = " "):
         # creates player object
@@ -171,8 +197,10 @@ class Player(Deck):
         """ String representation of player object"""
         return self.name
 
-    def response(self, pinput):
-        return pinput
+    def receive_card(self, card):
+        # player receives card from deck or player
+        self.hand.append(card)
+        self.get_player_hand()
 
     def sort_hand(self):
         """ sorts players hand by value (2's to Aces)"""
@@ -184,27 +212,21 @@ class Player(Deck):
                 if self.hand[j][0] == 1:
                     self.hand.append(self.hand[i])
                 else:
-                    self.hand[j][0] == j:
+                    self.hand[j][0] == j
                     self.hand.insert(i, self.hand[i])
                 j -= 1
+        
+    def remove_cards_from_hand(self, cards):
+        # removes card from hand
+        self.get_player_hand()
+        for i in range(len(self.hand)):
+            if cards in self.hand:
+                self.hand.pop(cards)
 
-    def receive_card(self, card):
-        # player receives card from deck or player
-        self.hand.append(card)
-        pass
-
-    def sit_at_table(self): ## rename, prep
-        # places player at table object
-        pass
-
-    def get_next_move(self, card, other_player):
-        # players next move, asks for card
-        return "hey %s, do you have any %s?", (other_player, card)
-
-    def give_card_to_player(self, cards):
-        # give requested card to player[i]
-        if card in self.hand:
-            self.hand.pop(card)
+    def get_player_hand(self):
+        for n in enumerate(self.hand):
+            self.hand[n][n] = [n][0]
+        return self.hand
 
     def count_books(self):
         # counts number of "books"
